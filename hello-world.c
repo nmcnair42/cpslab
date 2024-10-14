c9292e7a5e64ebb6d1e4f4136858773c60a1626b
nmcnair <nmcnair@miners.utep.edu>
2024-10-14 15:49:11 -0600

    first commit

---
+++ hello-world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// This is a comment
+/* 
+This is a c program that outputs "Hello World!"
+Date: 9.30.2024
+Author: cos-5401/6401
+*/
+
+int main () {
+    printf("Hello World!\n"):
+
+    return 0;
+
+}
