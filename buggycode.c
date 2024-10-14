6fb7b7b64b16e257704f8a779d638303c1976196
nmcnair <nmcnair@miners.utep.edu>
2024-10-14 16:25:14 -0600

    problem 1

---
+++ buggycode.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+ float fahrenheit;
+
+ printf("Enter temperature in Fahrenheit: ");
+
+ scanf("%f", &fahrenheit);
+
+ float celsius = (fahrenheit - 32) * 5 / 9;
+
+ printf("Temperature in Celsius: %f \n", 
+celsius);
+
+ return 0;
+
+}
