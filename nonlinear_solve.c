4971b4d8262c24bf353e45c1070c4073d485f062
nmcnair <nmcnair@miners.utep.edu>
2024-10-23 16:57:23 -0600

    bisection done

--- nonlinear_solve.c
+++ nonlinear_solve.c
@@ -71,11 +71,13 @@ int sign(double x) {
 
 double bisection (int a, int b) {
     
-    double c, fb, fa, w;
+    double c, fb, fa, w, a_, b_;
     int counter = 0;
 
-    fb = f(b);
-    fa = f(a);
+    double a_ = a;
+    double b_ = b;
+    double fb = f(b);
+    double fa = f(a);
 
     printf("Function values: lower:%f, upper:%f\n", fa, fb);
 
@@ -85,20 +87,20 @@ double bisection (int a, int b) {
     }
 
     while (counter < maxIter) {
-        c = (double)(b+a)/2;
+        c = (b_+a_)/2;
         w = f(c);
         printf("Midpoint %d: x=%f, f(x)=%f\n", counter, c, w );
 
-        if ( (fabs(b-a) < tol) || fabs(w) < error ) {
+        if ( (fabs(b_-a_) < tol) || fabs(w) < error ) {
             break;
         }
 
-        if ( sign(fb) != sign(fa) ) {
-            b = c;
-            fb = w;
-        } else {
-            a = c;
+        if ( w*fa > 0 ) {
+            a_ = c;
             fa = w;
+        } else {
+            b_ = c;
+            fb = w;
         }
 
         counter += 1;
