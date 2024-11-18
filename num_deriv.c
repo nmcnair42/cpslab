50743d6b734be9bca958f234684971d934e94855
nmcnair <nmcnair@miners.utep.edu>
2024-11-18 16:25:40 -0700

    done

--- num_deriv.c
+++ num_deriv.c
@@ -20,18 +20,22 @@ int main (int argc, char *argv[]) {
     double b = 2*M_PI;  // fix pi
     double a = 0.0;
 
-
     int n = atoi(argv[1]);
     printf("The value of n is %d\n", n);
     printf("The value of b is %f\n", b);
 
     double h = (b-a)/n;
 
-    double *act_deriv = malloc(sizeof(double)*n);
-    double *num_deriv = malloc(sizeof(double)*n);
-    double *x_vals = malloc(sizeof(double)*n);
+    double *act_deriv = malloc(sizeof(double)*n); // actual solution
+    double *num_deriv = malloc(sizeof(double)*n); // approximated solution
+    double *x_vals = malloc(sizeof(double)*n); // x values
 
     printf("The value of h is %f\n", h);
+
+    // choose method
+    int input;
+    printf("\nInput [1] to use forward difference method or\n[2] to use central difference:\n");
+    scanf("%d", &input);
 
     for (int i=0; i<n; i++) {
         x_vals[i] = (i+1)*h;
@@ -43,11 +47,22 @@ int main (int argc, char *argv[]) {
         printf("Actual derivative [%d] is %f\n", i, act_deriv[i]);
     }
 
-    printf("in function: \n");
-    central(x_vals, num_deriv, n, h);
+    switch(input) {
+        case 1:
+            printf("Using forward difference:\n");
+            forward(x_vals, num_deriv, n, h);
+            for (int i=0; i<n; i++) {
+                printf("Numerical derivative [%d] is %f\n", i, num_deriv[i]);
+            }
+            break;
 
-    for (int i=0; i<n; i++) {
-        printf("Numerical derivative [%d] is %f\n", i, num_deriv[i]);
+        case 2:
+            printf("Using central difference: \n");
+            central(x_vals, num_deriv, n, h);
+            for (int i=0; i<n; i++) {
+                printf("Numerical derivative [%d] is %f\n", i, num_deriv[i]);
+            }
+            break;
     }
 
     free(act_deriv);
@@ -65,7 +80,7 @@ double forward(double *x, double *result, int N, double H) {
         //x[i] = (i+1)*H;
         //printf("Value %d is %f\n", i, result[i]);
     }
-    result[N] = (funct(x[N]) - funct(x[N-1]))/H;
+    result[N] = (funct(x[N]) - funct(x[N-1]))/H; // using final two points
 }
 
 
@@ -75,7 +90,7 @@ double central(double *x, double *result, int N, double H) {
         //x[i] = (i+1)*H;
         //printf("Value %d is %f\n", i, result[i]);
     }
-    result[0] = (funct(x[1]) - funct(x[0]))/H;
+    result[0] = (funct(x[1]) - funct(x[0]))/H; // using first and last two points
     result[N] = (funct(x[N]) - funct(x[N-1]))/H;
 }
 
